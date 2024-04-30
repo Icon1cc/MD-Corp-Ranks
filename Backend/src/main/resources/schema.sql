f8aad4ecf54ded91f7e4694b6041264f54bc9f80
Simon Fladen Lie <73124270+Simonlie0302@users.noreply.github.com>
2024-04-30 20:35:32 +0200

    First iteration of the database (#5)
    * First iteration of the database
    
    * Apparently user is reserved, so much wasted time

--- Backend/src/main/resources/schema.sql
+++ Backend/src/main/resources/schema.sql
@@ -1,5 +1,45 @@
-
-CREATE TABLE IF NOT EXISTS "user" (
+CREATE TABLE IF NOT EXISTS client (
     "UserID" UUID PRIMARY KEY,
     "RegistrationTime" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS company_name (
+    "UserID" UUID NOT NULL,
+    "RegistrationTime" TIMESTAMP NOT NULL,
+    "CompanyName" VARCHAR(100) NOT NULL,
+    PRIMARY KEY ("UserID", "RegistrationTime"),
+    FOREIGN KEY ("UserID") REFERENCES "client"("UserID")
+);
+
+CREATE TABLE IF NOT EXISTS question (
+    "ID" SERIAL PRIMARY KEY,
+    "Title" VARCHAR(255) NOT NULL,
+    "Subtitle" VARCHAR(255) NOT NULL,
+    "Weight" DECIMAL NOT NULL
+);
+
+-- Currently using UserID and timestamp to be 
+-- able to save multiple answers of the same question by the same user.
+CREATE TABLE IF NOT EXISTS question_rating (
+    "UserID" UUID NOT NULL,
+    "QuestionID" INT NOT NULL,
+    "Rating" INT NOT NULL,
+    "Timestamp" TIMESTAMP NOT NULL,
+    PRIMARY KEY ("UserID", "Timestamp"),
+    FOREIGN KEY ("UserID") REFERENCES "client"("UserID"),
+    FOREIGN KEY ("QuestionID") REFERENCES "question"("ID")
+);
+
+CREATE TABLE IF NOT EXISTS page_exit (
+    "UserID" UUID NOT NULL,
+    "Timestamp" TIMESTAMP NOT NULL,
+    PRIMARY KEY ("UserID", "Timestamp"),
+    FOREIGN KEY ("UserID") REFERENCES "client"("UserID")
+);
+
+CREATE TABLE IF NOT EXISTS email_subscription (
+    "UserID" UUID PRIMARY KEY,
+    "Email" VARCHAR(255) NOT NULL,
+    "SubscribedOn" TIMESTAMP NOT NULL,
+    FOREIGN KEY ("UserID") REFERENCES "client"("UserID")
 );
