f0d1828aa435fd89d13a7e588390260c25b0071d
Simon Fladen Lie <73124270+Simonlie0302@users.noreply.github.com>
2024-04-30 16:41:29 +0200

    Added initial postgres boilerplate (#4)

--- Backend/src/main/java/com/mdCorpRanks/Backend/BackendApplication.java
+++ Backend/src/main/java/com/mdCorpRanks/Backend/BackendApplication.java
@@ -2,12 +2,26 @@ package com.mdCorpRanks.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import lombok.extern.java.Log;
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
-public class BackendApplication {
+@Log
+@RequiredArgsConstructor
+public class BackendApplication implements CommandLineRunner{
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    private final JdbcTemplate jdbcTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Override
+    public void run(final String... args) {
+        log.info("Executing startup actions...");
+        jdbcTemplate.execute("select * from user");
+    }
 }
+
---
+++ Backend/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE IF NOT EXISTS "user" (
+    "UserID" UUID PRIMARY KEY,
+    "RegistrationTime" TIMESTAMP NOT NULL
+);
