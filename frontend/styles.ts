e79d1bf04f91439836ebc4df1a1eeb0ede34bc2e
Rishabh Tiwari <rishtiwari98@gmail.com>
2024-05-09 01:05:14 +0200

    Updated tailwind.css function

---
+++ frontend/styles.ts
@@ -0,0 +1,71 @@
+export const styles = {
+    colors: {
+        primary: '#306a79',
+        secondary: '#004d66',
+        hoverSecondary: '#006680',
+        background: '#ffffff',
+        lightBackground: '#f3f3f3',
+        darkBackground: '#242424',
+        headerBackground: 'rgb(215, 206, 206)',
+        errorBackground: '#007a99',
+        borderColor: '#3498db',
+        disabled: '#cccccc',
+        disabledText: '#666666',
+        black: '#000000',
+        white: '#ffffff',
+        linkColor: '#646cff',
+        linkHover: '#535bf2',
+        error: '#F44336',
+        success: '#4CAF50',
+        warning: '#FFEB3B',
+        info: '#2196F3',
+        text: 'rgba(255, 255, 255, 0.87)',
+        textDark: '#213547',
+    },
+    fonts: {
+        base: 'Arial, sans-serif',
+        header: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+        body: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
+    },
+    boxShadow: {
+        light: '0 2px 4px rgba(0, 0, 0, 0.2)',
+        heavy: '0 4px 20px rgba(0, 0, 0, 0.5)',
+    },
+    borderRadius: {
+        small: '10px',
+        medium: '20px',
+        circle: '50%',
+    },
+    sizes: {
+        button: {
+            width: '300px',
+            padding: '10px',
+            fontSize: '1rem',
+            fontWeight: 'bold',
+        },
+        input: {
+            width: '70%',
+            padding: '1rem',
+            fontSize: '1rem',
+            marginBottom: '1rem',
+        },
+        modal: {
+            width: '90%',
+            height: '60%',
+            marginTop: '50px',
+        },
+        loader: {
+            width: '50px',
+            height: '50px',
+        },
+    },
+    text: {
+        headerSize: '60px',
+        baseSize: '30px',
+        smallSize: '16px',
+        thankYouSize: '2rem',
+        interestSize: '1.25rem',
+    }
+};
+
+export default styles;
