53e1de5a63436150763e0d1343ddd03937977fa2
Rishabh Tiwari <rishtiwari98@gmail.com>
2024-05-09 02:09:57 +0200

    added tailwind configuration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#306a79',
+        secondary: '#004d66',
+        hoverSecondary: '#006680',
+        background: '#ffffff',
+        lightBackground: '#f3f3f3',
+        darkBackground: '#242424',
+        headerBackground: 'rgb(215, 206, 206)',
+        errorBackground: '#007a99',
+        borderColor: '#3498db',
+        disabled: '#cccccc',
+        disabledText: '#666666',
+        black: '#000000',
+        white: '#ffffff',
+        linkColor: '#646cff',
+        linkHover: '#535bf2',
+        error: '#F44336',
+        success: '#4CAF50',
+        warning: '#FFEB3B',
+        info: '#2196F3',
+        text: 'rgba(255, 255, 255, 0.87)',
+        textDark: '#213547',
+      }
+    },
+  },
+  plugins: [],
+}
