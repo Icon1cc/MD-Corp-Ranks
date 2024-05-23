21a8383bfa67ca6080f8d02f37929d4ef1f294d7
Rishabh Tiwari <rishtiwari98@gmail.com>
2024-05-23 14:54:47 +0200

    Updated MVPs

--- MVP_2/frontend/src/App.tsx
+++ MVP_2/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import ReviewWizard from './pages/ReviewWizard';
+import ReviewWizardContainer from './components/ReviewWizardContainer';
 import ReviewAlreadyGiven from './components/ReviewAlreadyGiven';
 import ThankYou from './pages/ThankYou';
 import './styles/App.css';
@@ -12,7 +12,7 @@ const App: React.FC = () => {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/review" element={<ReviewWizard />} />
+          <Route path="/review" element={<ReviewWizardContainer />} />
           <Route path="/thank-you" element={<ThankYou />} />
           <Route path="/review-already-given" element={<ReviewAlreadyGiven />} />
         </Routes>
